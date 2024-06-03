09224b00c855a8bca95914157732a380ba428f0d
TechLion Dev (via GIT) <git@techlion.dev>
2024-06-03 18:52:48 -0400

    Fix Coptic Date Gregorian Date and Icon Explanation

--- utils/formatRecord.ts
+++ utils/formatRecord.ts
@@ -1,36 +1,43 @@
-import getIcons from './getIcons';
-import getStories from './getStories';
-import getFacts from './getFacts';
-import { getReadings } from './getReadings';
-import getUpcomingEvents from './getUpcomingEvents';
+import getIcons from "./getIcons";
+import getStories from "./getStories";
+import getFacts from "./getFacts";
+import { getReadings } from "./getReadings";
+import getUpcomingEvents from "./getUpcomingEvents";
+import { RecordModel } from "pocketbase";
 
 /**
  * Formats a record into a specific data structure.
  * @param record - The record to be formatted.
  * @returns {Promise<Data>} A Promise that resolves to the formatted data.
  */
-export default async function formatRecord(record, upcoming): Promise<Data> {
-    let formattedRecord = {
-        created: new Date(record.created),
-        date: new Date(record.date),
-        copticDate: {
-            created: new Date(record.expand.copticDate?.created),
-            day: record.expand.copticDate?.day,
-            id: record.expand.copticDate?.id,
-            month: record.expand.copticDate?.month,
-            updated: new Date(record.expand.copticDate?.updated),
-        },
-        icons: await getIcons(record.expand.icons),
-        stories: await getStories(record.expand.stories),
-        facts: await getFacts(record.expand.facts),
-        id: record.id,
-        liturgicalInformation: record.liturgicalInformation,
-        name: record.name,
-        updated: new Date(record.updated),
-        readings: await getReadings(new Date(record.date)),
-        isWellKnown: record.isWellKnown,
-        upcomingEvents: upcoming ? await getUpcomingEvents(new Date(record.date)) : [],
-    }
+export default async function formatRecord(
+  record: RecordModel,
+  upcoming: boolean,
+): Promise<Data> {
+  let formattedRecord = {
+    created: new Date(record.created),
+    date: new Date(record.date),
+    copticDate: {
+      created: new Date(record.expand.copticDate?.created),
+      day: record.expand.copticDate?.day,
+      id: record.expand.copticDate?.id,
+      month: record.expand.copticDate?.month,
+      updated: new Date(record.expand.copticDate?.updated),
+      gregorianDate: new Date(record.expand.copticDate?.gregorianDate),
+    },
+    icons: await getIcons(record.expand.icons),
+    stories: await getStories(record.expand.stories),
+    facts: await getFacts(record.expand.facts),
+    id: record.id,
+    liturgicalInformation: record.liturgicalInformation,
+    name: record.name,
+    updated: new Date(record.updated),
+    readings: await getReadings(new Date(record.date)),
+    isWellKnown: record.isWellKnown,
+    upcomingEvents: upcoming
+      ? await getUpcomingEvents(new Date(record.date))
+      : [],
+  };
 
-    return formattedRecord;
+  return formattedRecord;
 }
--- utils/getIcons.ts
+++ utils/getIcons.ts
@@ -1,28 +1,29 @@
-import generateFileUrl from './generateFileUrl';
-import generateCroppedFileUrl from './generateCroppedFileUrl';
-import getIconagrapher from './getIconagrapher';
+import generateFileUrl from "./generateFileUrl";
+import generateCroppedFileUrl from "./generateCroppedFileUrl";
+import getIconagrapher from "./getIconagrapher";
 
 /**
  * Retrieves the formatted icons based on the provided icons array.
  * @param {Icon[]} icons - The array of icons to be formatted.
  * @returns {Promise<Icon[]>} - The formatted icons array.
  */
-export default async function getIcons(icons) {
-    if (!icons) return [];
-    let formattedIcons = [];
-    for (let icon of icons) {
-        let formattedIcon = {
-            created: new Date(icon.created),
-            id: icon.id,
-            name: icon.name,
-            updated: new Date(icon.updated),
-            caption: icon.caption,
-            story: icon.story,
-            image: await generateFileUrl(icon),
-            croppedImage: await generateCroppedFileUrl(icon),
-            iconagrapher: await getIconagrapher(icon.iconographer),
-        }
-        formattedIcons.push(formattedIcon);
-    }
-    return formattedIcons;
+export default async function getIcons(icons: any[]): Promise<Icon[]> {
+  if (!icons) return [];
+  let formattedIcons = [];
+  for (let icon of icons) {
+    let formattedIcon = {
+      created: new Date(icon.created),
+      id: icon.id,
+      name: icon.name,
+      updated: new Date(icon.updated),
+      caption: icon.caption,
+      explanation: icon.explanation,
+      story: icon.story,
+      image: await generateFileUrl(icon),
+      croppedImage: await generateCroppedFileUrl(icon),
+      iconagrapher: await getIconagrapher(icon.iconographer),
+    };
+    formattedIcons.push(formattedIcon);
+  }
+  return formattedIcons;
 }
