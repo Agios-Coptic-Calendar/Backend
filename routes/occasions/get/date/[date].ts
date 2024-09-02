2c18119167905ae6816cdd4074a05cd9de6e2587
TechLion Dev (via GIT) <git@techlion.dev>
2024-09-02 09:31:02 -0400

    date

---
+++ routes/occasions/get/date/[date].ts
@@ -0,0 +1,43 @@
+import PocketBase from "pocketbase";
+
+const pb = new PocketBase("https://agios-calendar.pockethost.io");
+
+import formatRecord from "../../../../utils/formatRecord";
+
+/**
+ * Defines the event handler for retrieving an occasion by its ID.
+ * @param event The event object.
+ * @returns A Promise that resolves to a TLD_Response object.
+ */
+export default defineEventHandler(async (event) => {
+  setResponseHeaders(event, {
+    "Access-Control-Allow-Methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Credentials": "true",
+    "Access-Control-Allow-Headers": "*",
+    "Access-Control-Expose-Headers": "*",
+  });
+  if (getMethod(event) === "OPTIONS") {
+    event.res.statusCode = 204;
+    event.res.statusMessage = "No Content.";
+    return "OK";
+  }
+  const date = getRouterParam(event, "date");
+  let record = await pb
+    .collection("occasion")
+    .getFirstListItem(`date ?~ "${date}"`, {
+      expand: "copticDate,facts,icons,stories,icons.story",
+    });
+  let formattedRecord = await formatRecord(record);
+  if (!record) {
+    return {
+      status: 404,
+      statusText: "Not Found",
+    };
+  }
+  return {
+    data: formattedRecord,
+    status: 200,
+    statusText: "OK",
+  };
+});
